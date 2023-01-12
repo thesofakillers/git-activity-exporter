e634b08d5241da158bd3f24af3c1297ea237a34b
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-12 23:33:17 +0100

    fix for loops

--- git-activity-exporter.sh
+++ git-activity-exporter.sh
@@ -59,31 +59,33 @@ fi
 # }}}
 
 # Initialize the target folder as a git repository
+mkdir -p $target
 cd $target
 git init
 
 if [ -z "$branches" ]; then
   # Copy all commits from the source repository to the target repository
   # Only the dates and author are copied
-  for date in $(git --git-dir=$source/.git log \
-    --pretty=format:%ad --author=$author); \
-  do
-    git commit --date="$date" --author="$author" --allow-empty --allow-empty-message -m "$commit_message"
+  git --git-dir=$source/.git log --pretty=format:%ad --author="$author" \
+  | while read -r cdate ; do
+    git commit --date="$cdate" --author="$author" --allow-empty \
+      --allow-empty-message -m "$commit_message"
   done
 else
   IFS=' ' read -ra branch_list <<< "$branches"
   for branch in "${branch_list[@]}"; do
     # Copy the commits from the specified branch in the source repository to the target repository
     # Only the dates and author are copied
-    for commit_hash in $(git --git-dir=$source/.git rev-list $branch \
+    git --git-dir=$source/.git rev-list \
+      $branch --author="$author"\
       | git --git-dir=$source/.git name-rev --stdin \ 
       | sed -E 's/~[0-9]+//g; s/\^[0-9]+//g'\
       | grep " <$branch>" \
-      | awk -F " " '{print $1}'); \
-    do
-      date=$(git --git-dir=$source/.git show -s --format=%ad $commit_hash)
-      git commit --date="$date" --author="$author" --allow-empty \
-        --allow-empty-message -m "$commit_message"
+      | awk -F " " '{print $1}' \
+      | while read -r commit_hash ; do
+        cdate=$(git --git-dir=$source/.git show -s --format=%ad $commit_hash)
+        git commit --date="$cdate" --author="$author" --allow-empty \
+          --allow-empty-message -m "$commit_message"
     done
   done
 fi
